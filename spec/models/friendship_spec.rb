f4f3562b2f7c46245f799dd0b880d2e92ed2c83b
Bismarck-GM <geromorla@yahoo.com.ar>
2020-08-24 17:20:29 -0300

    Fix rubocop offenses

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -13,7 +13,11 @@ RSpec.describe Friendship, type: :model do
 
   describe 'validations' do
     subject { Friendship.create(user_id: @sender.id, friend_id: @receiver.id, accepted: false) }
-    it { should validate_uniqueness_of(:user_id).scoped_to(:friend_id).with_message('You\'ve already sent a friend request to this person') }
+    it {
+      should validate_uniqueness_of(:user_id)
+        .scoped_to(:friend_id)
+        .with_message('You\'ve already sent a friend request to this person')
+    }
   end
 
   describe 'custom validations' do
