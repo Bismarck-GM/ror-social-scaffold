9c24a9f3c25a1e6aa99cad7f7c2dbb69aa3f7986
Bismarck-GM <geromorla@yahoo.com.ar>
2020-08-25 13:00:50 -0300

    Delete config hosts from application config.

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module RorSocialScaffold
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.hosts << 'naked-social-scaffold.herokuapp.com'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
